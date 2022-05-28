8ff071320923434a314760aa4ef9daa80f69003c
swaraj kumar <86548075+swarajkumar001@users.noreply.github.com>
2022-05-28 22:03:36 +0530

    Add files via upload

---
+++ 1492. The kth Factor of n.cpp
@@ -0,0 +1,59 @@
+/*You are given two positive integers n and k.
+ A factor of an integer n is defined as an integer i where n % i == 0.
+
+Consider a list of all factors of n sorted in ascending order, return
+ the kth factor in this list or return -1 if n has less than k factors.
+
+ 
+
+Example 1:
+
+Input: n = 12, k = 3
+Output: 3
+Explanation: Factors list is [1, 2, 3, 4, 6, 12], the 3rd factor is 3.
+Example 2:
+
+Input: n = 7, k = 2
+Output: 7
+Explanation: Factors list is [1, 7], the 2nd factor is 7.
+Example 3:
+
+Input: n = 4, k = 4
+Output: -1
+Explanation: Factors list is [1, 2, 4], there is only 3 factors. We should return -1.
+ 
+
+Constraints:
+
+1 <= k <= n <= 1000*/
+
+//CODE 
+
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+        vector<int>v;
+        
+         for(int i=1;i<=n;i++){
+             
+             if(n%i==0){
+                 v.push_back(i);
+             }
+             
+         }
+        
+        int r=v.size();
+        
+        
+        if(r>=k){
+            return v[k-1];
+        }
+        else{
+            return -1;
+        }
+        
+        
+        
+    }
+};
+ 
